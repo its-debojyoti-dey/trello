326e04e7cfe8e06db4a68a73430ba3b7558f324d
its-debojyoti-dey <its-debojyoti-dey@users.noreply.github.com>
2026-07-05 10:27:53 +0530

    chore: add allowedDevOrigins to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['ascendo.debojyotidey.online'],
   turbopack: {
     root: path.join(__dirname),
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
